b490e7392ef116d8997d6a42b8b542390e2c86ff
Lavrentev <94319426+Lavrentev10@users.noreply.github.com>
2022-09-04 12:41:01 +0300

    Add files via upload

---
+++ Task1/Lavrentev_Task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    cout << "Hello world" << endl;
+
+    cout << "Enter your name... ";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!" <<endl;
+
+}
